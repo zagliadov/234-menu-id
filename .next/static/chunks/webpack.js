a7da1711f002977bbefd0d9cc6021280ea6bf806
Daniil <daniil@MacBook-Pro-Ilona.local>
2021-11-10 14:04:04 +0200

    fix text

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "85ed8affcf336894"; }
+/******/ 		__webpack_require__.h = function() { return "7c54e2720da6d649"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/BottomPart/components/WhatCanYou/WhatCanYou.js
+++ components/BottomPart/components/WhatCanYou/WhatCanYou.js
@@ -6,15 +6,25 @@ import {
 export const WhatCanYou = () => {
 
     return (
-        <Text 
-        fontFamily='Poppins'
-        fontStyle='normal'
-        fontWeight='600'
-        fontSize='16px'
-        lineHeight='24px'
-        mt='12px'
-        >
+        <p style={{
+            fontFamily: 'Poppins',
+            fontStyle: 'normal',
+            fontWeight: '600',
+            fontSize:'16px',
+            lineHeight: '24px',
+            marginTop: '12px',
+        }}>
             How can you help with this Requset?
-        </Text>
+        </p>
+        // <Text 
+        // fontFamily='Poppins'
+        // fontStyle='normal'
+        // fontWeight='600'
+        // fontSize='16px'
+        // lineHeight='24px'
+        // mt='12px'
+        // >
+        //     How can you help with this Requset?
+        // </Text>
     )
 }
