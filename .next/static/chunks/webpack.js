72ed649b1d80e28f03d6a0c39827e39758d55407
Daniil <daniil@MacBook-Pro-Ilona.local>
2021-11-10 13:06:01 +0200

    text color fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b697f3bd4554b08e"; }
+/******/ 		__webpack_require__.h = function() { return "25cae9c9d6688b1a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/TopPart/components/MyQuestion/MyQuestion.js
+++ components/TopPart/components/MyQuestion/MyQuestion.js
@@ -14,22 +14,22 @@ export const MyQuestion = () => {
             flexDir="column"
             mt='24px'
             color='#fffff'
-            opacity='0.8'
             fontStyle='normal'
             lineHeight='25px'
             alignSelf='stretch'
             fontSize='18px'
             letterSpacing='0.3px'
-            fontWeight='600'
+            fontWeight='900'
         >
             <Text
+            fontWeight='900'
             >
                 Velit ut ultrices quis viverra eu, ultricies nulla at nec.
                 Ut diam venenatis egestas massa vulputate nam. Pretium eros, imperdiet odio sit.
                 Natoque quam mi ut leo. Sed ut sit cursus nunc, sit. Magna neque vel amet sem vulputate lacus ut.
 
             </Text>
-            <Text mt='24px' letterSpacing='0.301px'>
+            <Text mt='24px' letterSpacing='0.301px' fontWeight='900'>
                 Diam lacus sed ornare vulputate. Vulputate magna id suspendisse aliquam.
                 Sit fames est proin diam morbi purus non. Purus donec eu arcu euismod. Volutpat facilisi venenatis phasellus maecenas in.
             </Text>
